b40f56a9bb3a12ebe2d4ca4734e4d6678895033a
Gieryn <gierynt@gmail.com>
2017-11-20 23:17:26 +0100

    still not working

--- script.js
+++ script.js
@@ -5,25 +5,25 @@ var movies = [
         id: 1,
         title: 'Harry Potter',
         desc: 'Film o czarodziejach',
-        image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRt3xCYs6Tbn-aDz1k_8obBFh9c6cZXQLqmtHhSF10AJcIqW3f2kQ'
+        imageSrc: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRt3xCYs6Tbn-aDz1k_8obBFh9c6cZXQLqmtHhSF10AJcIqW3f2kQ'
     },
     {
         id: 2,
         title: 'Lion King',
         desc: 'Film o lwach',
-        image: 'https://upload.wikimedia.org/wikipedia/en/thumb/3/3d/The_Lion_King_poster.jpg/220px-The_Lion_King_poster.jpg'
+        imageSrc: 'https://upload.wikimedia.org/wikipedia/en/thumb/3/3d/The_Lion_King_poster.jpg/220px-The_Lion_King_poster.jpg'
     },
     {
         id: 3,
         title: 'Ace Ventura',
         desc: 'Film o Jimie Carrey',
-        image: 'http://images.genius.com/436b2e5013668782fbbed1c8bb5f084a.591x591x1.jpg'
+        imageSrc: 'http://images.genius.com/436b2e5013668782fbbed1c8bb5f084a.591x591x1.jpg'
     },
     {
         id: 4,
         title: 'Wojna Światow',
         desc: 'Film o inwazji obcych',
-        image: 'https://lh6.ggpht.com/fOpyTFEutU6BfwiG0kzevES2pr1wLt42yIM4tSbQHtJZG4SyOOziNDvLhwm_c-53iD-MJw'
+        imageSrc: 'https://lh6.ggpht.com/fOpyTFEutU6BfwiG0kzevES2pr1wLt42yIM4tSbQHtJZG4SyOOziNDvLhwm_c-53iD-MJw'
     }
 ];
 
@@ -37,11 +37,12 @@ var Movie = React.createClass({
     },
 
     render: function () {
+        //this = self; 
         return (
             React.createElement('div', {},
-                React.createElement('h2', {}, this.props.movieObject.title),
-                React.createElement('p', {}, this.props.movieObject.desc),
-                React.createElement('img', { src: this.props.movieObject.image })
+                React.createElement(MovieTitle, {title: this.props.movieObject.title}),
+                React.createElement(MovieDescription, {desc: this.props.movieObject.desc}),
+                React.createElement(MovieImage, {imageSrc: this.props.movieObject.imageSrc})
             )
         );
     },
@@ -49,19 +50,84 @@ var Movie = React.createClass({
 });
 
 
-var moviesElements = movies.map(function (movie) {
-    
+var MovieTitle = React.createClass({
+
+    propTypes: {
+
+        title: React.PropTypes.string.isRequired,
+    },
+
+    render: function() {
+
         return (
-            React.createElement('li', {key: movie.id}),
-            React.createElement(Movie, {key: movie.id, movieObject: movie})
+            React.createElement('h2', {}, this.props.title)
         );
-    
-    });
+    },
+});
+
+
+var MovieDescription = React.createClass({
+
+    propTypes: {
+
+        desc: React.PropTypes.string.isRequired,    
+    },
+
+    render: function() {
+
+        return (
+            React.createElement('p', {}, this.props.desc)
+        );
+    },
+});
+
+
+var MovieImage = React.createClass({
+
+    propTypes: {
+
+        imageSrc: React.PropTypes.string.isRequired, 
+    },
+
+    render: function() {
+
+        return (
+            React.createElement('img', {src: this.props.imageSrc})
+        );
+    },
+});
+
+
+var MoviesList = React.createClass({
+
+    propTypes: {
+
+        movies: React.PropTypes.array.isRequired,
+    },
+
+    render: function () {
+
+
+        movies.map(function (movie) {
+
+            React.createElement('li', { key: movie.id },
+                React.createElement(Movie, { movieObject: movie })
+
+            );
+        });
+
+    },
+
+}); 
+
+
 
 var element = 
     React.createElement('div', {}, 
         React.createElement('h1', {}, 'Lista filmow'),
-        React.createElement('ul', {}, moviesElements)
+        React.createElement('ul', {}, 
+            React.createElement(MoviesList, {movies:movies})
+        )
 );
 
 ReactDOM.render(element, document.getElementById('app'));
